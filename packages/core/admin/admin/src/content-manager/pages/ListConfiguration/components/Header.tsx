a9cc0552a778040a53a15a01f6bbd9ada3c08166
Josh <37798644+joshuaellis@users.noreply.github.com>
2024-02-06 08:17:31 +0000

    fix(cm): reimplement ListSettingsView

---
+++ packages/core/admin/admin/src/content-manager/pages/ListConfiguration/components/Header.tsx
@@ -0,0 +1,57 @@
+import { Button, HeaderLayout } from '@strapi/design-system';
+import { Link } from '@strapi/design-system/v2';
+import { ArrowLeft } from '@strapi/icons';
+import { useIntl } from 'react-intl';
+import { NavLink } from 'react-router-dom';
+
+import { capitalise } from '../../../../utils/strings';
+import { useForm } from '../../../components/Form';
+import { getTranslation } from '../../../utils/translations';
+
+interface HeaderProps {
+  collectionType: string;
+  name: string;
+  model: string;
+}
+
+const Header = ({ collectionType, name, model }: HeaderProps) => {
+  const { formatMessage } = useIntl();
+
+  const modified = useForm('Header', (state) => state.modified);
+  const isSubmitting = useForm('Header', (state) => state.isSubmitting);
+
+  return (
+    <HeaderLayout
+      navigationAction={
+        <Link
+          startIcon={<ArrowLeft />}
+          // @ts-expect-error invalid typings
+          to={`/content-manager/${collectionType}/${model}`}
+          id="go-back"
+          as={NavLink}
+        >
+          {formatMessage({ id: 'global.back', defaultMessage: 'Back' })}
+        </Link>
+      }
+      primaryAction={
+        <Button size="S" disabled={!modified} type="submit" loading={isSubmitting}>
+          {formatMessage({ id: 'global.save', defaultMessage: 'Save' })}
+        </Button>
+      }
+      subtitle={formatMessage({
+        id: getTranslation('components.SettingsViewWrapper.pluginHeader.description.list-settings'),
+        defaultMessage: 'Define the settings of the list view.',
+      })}
+      title={formatMessage(
+        {
+          id: getTranslation('components.SettingsViewWrapper.pluginHeader.title'),
+          defaultMessage: 'Configure the view - {name}',
+        },
+        { name: capitalise(name) }
+      )}
+    />
+  );
+};
+
+export { Header };
+export type { HeaderProps };
